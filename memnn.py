06e80387069ab0ae4f4b1d972ada5b270cb82760
jojonki <junki.ohmura@gmail.com>
2017-12-26 22:20:41 -0500

    tiny changes

--- memnn.py
+++ memnn.py
@@ -14,6 +14,9 @@ class MemNN(nn.Module):
         self.A = nn.ModuleList([nn.Embedding(vocab_size, embd_size) for _ in range(hops+1)])
         for i in range(len(self.A)):
             self.A[i].weight = nn.Parameter(torch.randn(vocab_size, embd_size).normal_(-init_rng, init_rng))
+        self.B = self.A[0] # query encoder
+
+        # Temporal Encoding: see 4.1
         self.TA = nn.Parameter(torch.randn(1, story_len, 1).normal_(-init_rng, init_rng))
         self.TC = nn.Parameter(torch.randn(1, story_len, 1).normal_(-init_rng, init_rng))
 
@@ -27,9 +30,10 @@ class MemNN(nn.Module):
 
         x = x.view(bs*story_len, -1) # (bs*s_sent_len, s_sent_len)
 
-        u = self.dropout(self.A[1](q)) # (bs, q_sent_len, embd_size)
+        u = self.dropout(self.B(q)) # (bs, q_sent_len, embd_size)
         u = torch.sum(u, 1) # (bs, embd_size)
 
+        # Adjacent weight tying
         for k in range(self.hops):
             m = self.dropout(self.A[k](x)) # (bs*story_len, s_sent_len, embd_size)
             m = m.view(bs, story_len, s_sent_len, -1) # (bs, story_len, s_sent_len, embd_size)
@@ -41,11 +45,13 @@ class MemNN(nn.Module):
             c = torch.sum(c, 2) # (bs, story_len, embd_size)
             c += self.TC.repeat(bs, 1, self.embd_size)
 
-            p = torch.bmm(m, u.unsqueeze(2)) # (bs, story_len, 1)
-            p = F.softmax(p.squeeze()).unsqueeze(2)
+            p = torch.bmm(m, u.unsqueeze(2)).squeeze() # (bs, story_len)
+            p = F.softmax(p, -1) # (bs, story_len)
+            p = p.unsqueeze(2).repeat(1, 1, self.embd_size) # (bs, story_len, embd_size)
             o = c * p # use m as c, (bs, story_len, embd_size)
             o = torch.sum(o, 1) # (bs, embd_size)
             u = o + u # (bs, embd_size)
+
         W = torch.t(self.A[-1].weight) # (embd_size, vocab_size)
         out = torch.bmm(u.unsqueeze(1), W.unsqueeze(0).repeat(bs, 1, 1)).squeeze() # (bs, ans_size)
         return F.log_softmax(out)
